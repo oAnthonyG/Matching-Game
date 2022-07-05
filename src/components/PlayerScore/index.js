09866262a6e480be7433eccdbb4919f3ecc6f5ea
GPC <anthonytguilherme@hotmail.com>
2022-07-05 16:14:29 -0300

    create html of the playerScore component

---
+++ src/components/PlayerScore/index.js
@@ -0,0 +1,14 @@
+function PlayerScore(points = 0){
+    return /*html*/`
+        <ol data-points="${points}">
+            <li>um</li>
+            <li>dois</li>
+            <li>tres</li>
+        </ol>
+    
+    
+    `
+
+}
+
+export default PlayerScore
