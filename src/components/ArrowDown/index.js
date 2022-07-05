b2fe86460b2b3e1d4e1bf3bfef385790ce483916
GPC <anthonytguilherme@hotmail.com>
2022-07-05 17:55:16 -0300

    add icon arrowDown pgn

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,9 @@
+function ArrowDown() {
+    return /*html*/`
+    <img src="./src/components/images/IconArrowDown.png" alt="icone de uma seta para baixo">
+    
+    ` 
+
+}
+
+export default ArrowDown
